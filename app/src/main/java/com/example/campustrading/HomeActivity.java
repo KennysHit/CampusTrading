28d0c2f2041e279afd31e857427641aae89d6b97
KennysHit <1239667935@qq.com>
2019-06-02 18:53:44 +0800

    完善底部导航控件和控制界面转换

--- app/src/main/java/com/example/campustrading/HomeActivity.java
+++ app/src/main/java/com/example/campustrading/HomeActivity.java
@@ -1,8 +1,15 @@
 package com.example.campustrading;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -11,6 +18,35 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_home );
 
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById ( R.id.bottom_navigation );
+        final LinearLayout linearLayout_home = (LinearLayout ) findViewById ( R.id.view_home );
+        final LinearLayout linearLayout_message = (LinearLayout ) findViewById ( R.id.view_message );
+        final LinearLayout linearLayout_person = (LinearLayout ) findViewById ( R.id.view_person );
 
+        bottomNavigationView.setOnNavigationItemReselectedListener ( new BottomNavigationView.OnNavigationItemReselectedListener ( ) {
+            @Override
+            public void onNavigationItemReselected ( @NonNull MenuItem menuItem ) {
+                switch ( menuItem.getItemId () ){
+                    case R.id.home:
+                        Toast.makeText ( HomeActivity.this,"home is clicked!",Toast.LENGTH_LONG ).show ();
+                        linearLayout_person.setVisibility ( View.INVISIBLE );
+                        linearLayout_message.setVisibility ( View.INVISIBLE );
+                        linearLayout_home.setVisibility ( View.VISIBLE );
+                        break;
+                    case R.id.message:
+                        Toast.makeText ( HomeActivity.this,"message is clicked!",Toast.LENGTH_LONG ).show ();
+                        linearLayout_person.setVisibility ( View.INVISIBLE );
+                        linearLayout_home.setVisibility ( View.INVISIBLE );
+                        linearLayout_message.setVisibility ( View.VISIBLE );
+                        break;
+                    case R.id.peroson:
+                        Toast.makeText ( HomeActivity.this,"person is clicked!",Toast.LENGTH_LONG ).show ();
+                        linearLayout_home.setVisibility ( View.INVISIBLE );
+                        linearLayout_message.setVisibility ( View.INVISIBLE );
+                        linearLayout_person.setVisibility ( View.VISIBLE );
+                        break;
+                }
+            }
+        } );
     }
 }
