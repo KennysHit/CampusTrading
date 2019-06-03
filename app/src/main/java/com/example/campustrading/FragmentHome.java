3c77fd8b418f8c0799c6bc499c47a799c496ed5e
KennysHit <1239667935@qq.com>
2019-06-03 13:50:54 +0800

    添加FragmentHome

---
+++ app/src/main/java/com/example/campustrading/FragmentHome.java
@@ -0,0 +1,66 @@
+package com.example.campustrading;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FragmentHome extends Fragment {
+    private List<ItemObject> data;
+    private RecyclerView itemList;
+    private ItemListAdapter itemListAdapter;
+    private View view;//定义view用来设置fragment的layout
+
+    @Nullable
+    @Override
+    public View onCreateView ( @NonNull LayoutInflater inflater , @Nullable ViewGroup container , @Nullable Bundle savedInstanceState ) {
+        view = inflater.inflate ( R.layout.fragment_home,container,false );
+        dataInit ();
+        initRecyclerView ();
+        return view;
+
+    }
+
+
+    public void dataInit(){
+        data = new ArrayList<ItemObject> (  );
+        ItemObject itemObject1 = new ItemObject ( "商品一","","100" );
+        ItemObject itemObject2 = new ItemObject ( "商品二","","200" );
+        ItemObject itemObject3 = new ItemObject ( "商品三","","300" );
+        data.add ( itemObject1 );
+        data.add ( itemObject2 );
+        data.add ( itemObject3 );
+    }
+    private void initRecyclerView() {
+        //获取RecyclerView
+        itemList =(RecyclerView)view.findViewById ( R.id.item_list );
+        //创建adapter
+        itemListAdapter = new ItemListAdapter (getActivity(), data);
+        itemList.setLayoutManager(new LinearLayoutManager(getActivity ()));
+        //给RecyclerView设置adapter
+        itemList.setAdapter(itemListAdapter);
+        //设置layoutManager,可以设置显示效果，是线性布局、grid布局，还是瀑布流布局
+        itemListAdapter.setOnItemClickListener ( new ItemListAdapter.OnItemClickListener ( ) {
+            @Override
+            public void onClick ( int position ) {
+                Toast.makeText ( getActivity (),"short click!",Toast.LENGTH_LONG ).show ();
+            }
+        } );
+        itemListAdapter.setOnItemLongClickListener ( new ItemListAdapter.OnItemLongClickListener ( ) {
+            @Override
+            public void onLongClick ( int position ) {
+                Toast.makeText ( getActivity (),"long click",Toast.LENGTH_LONG ).show ();
+            }
+        } );
+    }
+}
--- app/src/main/java/com/example/campustrading/HomeActivity.java
+++ app/src/main/java/com/example/campustrading/HomeActivity.java
@@ -2,85 +2,45 @@ package com.example.campustrading;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
-    private List<ItemObject> data;
-    private RecyclerView itemList;
-    private ItemListAdapter itemListAdapter;
-
     @Override
     protected void onCreate ( Bundle savedInstanceState ) {
         super.onCreate ( savedInstanceState );
-        setContentView ( R.layout.activity_home );
+        setContentView ( R.layout.layout_main );
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById ( R.id.bottom_navigation );
-        final LinearLayout linearLayout_home = (LinearLayout ) findViewById ( R.id.view_home );
-        final LinearLayout linearLayout_message = (LinearLayout ) findViewById ( R.id.view_message );
-        final LinearLayout linearLayout_person = (LinearLayout ) findViewById ( R.id.view_person );
-        dataInit ();
 
-        itemList = (RecyclerView )findViewById ( R.id.list_item ) ;
-        itemListAdapter = new ItemListAdapter ( this,data);
-        itemList.setLayoutManager ( new LinearLayoutManager ( this ) );//设置样式列表
-        itemList.setAdapter ( itemListAdapter );
-        itemListAdapter.setOnItemClickListener ( new ItemListAdapter.OnItemClickListener ( ) {
+
+        bottomNavigationView.setOnNavigationItemSelectedListener ( new BottomNavigationView.OnNavigationItemSelectedListener ( ) {
             @Override
-            public void onClick ( int position ) {
-                Toast.makeText ( HomeActivity.this,"short click!",Toast.LENGTH_LONG ).show ();
-            }
-        } );
-        itemListAdapter.setOnItemLongClickListener ( new ItemListAdapter.OnItemLongClickListener ( ) {
-            @Override
-            public void onLongClick ( int position ) {
-                Toast.makeText ( HomeActivity.this,"long click",Toast.LENGTH_LONG ).show ();
-            }
-        } );
-        bottomNavigationView.setOnNavigationItemReselectedListener ( new BottomNavigationView.OnNavigationItemReselectedListener ( ) {
-            @Override
-            public void onNavigationItemReselected ( @NonNull MenuItem menuItem ) {
+            public boolean onNavigationItemSelected ( @NonNull MenuItem menuItem ) {
                 switch ( menuItem.getItemId () ){
                     case R.id.home:
                         Toast.makeText ( HomeActivity.this,"home is clicked!",Toast.LENGTH_LONG ).show ();
-                        linearLayout_person.setVisibility ( View.INVISIBLE );
-                        linearLayout_message.setVisibility ( View.INVISIBLE );
-                        linearLayout_home.setVisibility ( View.VISIBLE );
+                        FragmentHome fragmentHome = new FragmentHome ();
+                        getSupportFragmentManager ().beginTransaction ().replace ( R.id.view_main,fragmentHome ).commit ();
                         break;
                     case R.id.message:
                         Toast.makeText ( HomeActivity.this,"message is clicked!",Toast.LENGTH_LONG ).show ();
-                        linearLayout_person.setVisibility ( View.INVISIBLE );
-                        linearLayout_home.setVisibility ( View.INVISIBLE );
-                        linearLayout_message.setVisibility ( View.VISIBLE );
+                        FragmentMessage fragmentMessage = new FragmentMessage ();
+                        getSupportFragmentManager ().beginTransaction ().replace ( R.id.view_main,fragmentMessage ).commit ();
                         break;
                     case R.id.peroson:
                         Toast.makeText ( HomeActivity.this,"person is clicked!",Toast.LENGTH_LONG ).show ();
-                        linearLayout_home.setVisibility ( View.INVISIBLE );
-                        linearLayout_message.setVisibility ( View.INVISIBLE );
-                        linearLayout_person.setVisibility ( View.VISIBLE );
+                        FragmentPerson fragmentPerson = new FragmentPerson ();
+                        getSupportFragmentManager ().beginTransaction ().replace ( R.id.view_main,fragmentPerson ).commit ();
                         break;
                 }
+                return true;
             }
         } );
     }
-    public void dataInit(){
-        data = new ArrayList<ItemObject> (  );
-        ItemObject itemObject1 = new ItemObject ( "商品一","","100" );
-        ItemObject itemObject2 = new ItemObject ( "商品二","","200" );
-        ItemObject itemObject3 = new ItemObject ( "商品三","","300" );
-        data.add ( itemObject1 );
-        data.add ( itemObject2 );
-        data.add ( itemObject3 );
-    }
+
 }
