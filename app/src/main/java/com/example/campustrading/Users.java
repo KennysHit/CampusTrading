2e8baa9afeeec2fc39b8660db738b93fc969cb36
groupfind <groupfindmomo@gmail.com>
2019-06-04 17:00:45 +0800

    user+mer

--- app/src/main/java/com/example/campustrading/Users.java
+++ app/src/main/java/com/example/campustrading/Users.java
@@ -2,24 +2,7 @@ package com.example.campustrading;
 
 public class Users {
 //    用户识别码
-class Id {
 
-    }
-//    电话
- class Tell{
 
-    }
-//    姓名
- class Name {
-
-    }
-//    学校
- class School {
-
-    }
-//    学号
-class School_Id {
-
-    }
 
 }
