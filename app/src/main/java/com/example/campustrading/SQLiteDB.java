7a3fda1d08baf5265acc2b8b15f8bb86fa6d6c6c
KennysHit <1239667935@qq.com>
2019-06-04 17:37:37 +0800

    添加SQLite类

--- app/src/main/java/com/example/campustrading/SQLiteDB.java
+++ app/src/main/java/com/example/campustrading/SQLiteDB.java
@@ -6,7 +6,7 @@ import android.database.sqlite.SQLiteDatabase;
 
 
 public class SQLiteDB {
-    public static String TEST_TABLE="create table Test_table( id integer primary key autoincrement,name text)";
+    public static String TEST_TABLE= "Test_table";
     private SQLiteDatabase db;
 
     public SQLiteDB (String dir){
@@ -15,7 +15,7 @@ public class SQLiteDB {
 
     private void createTable(String table){
         //执行SQL语句
-        db.execSQL(table);
+        db.execSQL("create table "+table+"( id integer primary key autoincrement,name text)");
     }
 
     /**
@@ -24,19 +24,34 @@ public class SQLiteDB {
      * @param name 待插入用户名
      */
     public void insert(SQLiteDatabase db,String name){
-        Cursor cursor = db.query ( "Test_table",null,null,null,null,null,null );
+        Cursor cursor = db.query ( TEST_TABLE,null,null,null,null,null,null );
         //插入数据SQL语句
-        ContentValues contentValues = new ContentValues (  );
-        cursor.moveToFirst ();
-        String sql="insert into stu_table(id,name) values('"+String.valueOf ( cursor.getLong ( 0 ) )+"','"+name+"')";
+        String sql="insert into stu_table(id,name) values('"+String.valueOf ( cursor.getCount ()+1)+"','"+name+"')";
         //执行SQL语句
         db.execSQL(sql);
     }
     public void deleteById(int id) {
         //删除SQL语句
-        String sql = "delete from Test_table where id = "+String.valueOf ( id );
+        String sql = "delete from " + TEST_TABLE + " where id = " + String.valueOf ( id );
         //执行SQL语句
         db.execSQL(sql);
     }
-    public
+    private User queryById(int id) {
+        User user = null;
+        //查询获得游标
+        Cursor cursor = db.query (TEST_TABLE,null,null,null,null,null,null);
+            //判断游标是否为空
+        if(cursor.moveToFirst()){
+            //遍历游标
+            for(int i=0;i<cursor.getCount();i++){
+                cursor.move(i);
+                //获得ID
+                user.setId ( cursor.getInt(0) );
+                //获得用户名
+                user.setName ( cursor.getString(1) );
+                System.out.println(user.getId ()+":"+user.getName ());
+            }
+        }
+        return user;
+    }
 }
